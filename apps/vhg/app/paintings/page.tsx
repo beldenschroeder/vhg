124e38f36348dbb46bc864c16148504380881bb4
Belden Schroeder <beldenschroeder@gmail.com>
2024-04-23 19:24:51 -0700

    VHG-29: Remove unused comments

--- apps/vhg/app/paintings/page.tsx
+++ apps/vhg/app/paintings/page.tsx
@@ -1,33 +1,10 @@
-// 'use client';
-// import styled from 'styled-components';
 import PaintingCollection from '../../components/painting-collection/painting-collection';
-
-/* eslint-disable-next-line */
-// export interface PaintingsProps {}
-
-// const StyledPaintings = styled.div`
-//   color: pink;
-// `;
 
 export default function Paintings() {
   return (
-    // <StyledPaintings>
     <div>
       <h1>Welcome to Paintings!</h1>
       <PaintingCollection />
     </div>
-    // </StyledPaintings>
   );
 }
-
-// export const getStaticProps: GetStaticProps<PaintingsProps> = async (
-//   context
-// ) => {
-//   return {
-//     props: {
-//       name: 'Juri',
-//     },
-//   };
-// };
-
-// export default Paintings;
