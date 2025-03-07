887c6a90b211c08febef3a2d9326773efa6d7b7d
Belden Schroeder <beldenschroeder@gmail.com>
2025-03-06 20:18:27 -0800

    VHG-25: Empty code project

--- apps/vhg/app/api/paintings/route.ts
+++
@@ -1,8 +0,0 @@
-import { paintingsRepo } from '@vhg/vhg-libs';
-import { NextResponse } from "next/server";
-
-export async function GET(req: Request) {
-  const feed = await paintingsRepo.getAll();
-
-  return NextResponse.json(feed);
-}
