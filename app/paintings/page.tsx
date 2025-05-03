244d393f52dc55fbc19d9b6fef65f6b7eb3c5fe5
Belden Schroeder <beldenschroeder@gmail.com>
2025-05-02 20:22:53 -0700

    VHG-37: Add ImageGrid

--- app/paintings/page.tsx
+++ app/paintings/page.tsx
@@ -1,7 +1,13 @@
 import Typography from '@mui/material/Typography';
+import ImageGrid from './components/ImageGrid';
 
 const Paintings = () => {
-  return <Typography component="p">Painting content.</Typography>;
+  return (
+    <>
+      <Typography component="p">Painting content.</Typography>
+      <ImageGrid />
+    </>
+  );
 };
 
 export default Paintings;
